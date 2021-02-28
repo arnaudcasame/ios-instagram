546652c7147bf9af016a41a5093bce17b7d9b81c
arnaudcasame <noon.tizekod@gmail.com>
2021-02-28 01:03:45 -0500

    Implements the login page of instagram

---
+++ Instagram/Resources/Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  Extensions.swift
+//  Instagram
+//
+//  Created by Arnaud Casame on 2/27/21.
+//
+
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
