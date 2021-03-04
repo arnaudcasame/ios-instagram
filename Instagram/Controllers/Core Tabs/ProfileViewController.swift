cadc64882421a326957ef7111d1404a35c29a78a
arnaudcasame <noon.tizekod@gmail.com>
2021-03-04 00:47:18 -0500

    Implements log out feature

--- Instagram/Controllers/Core Tabs/ProfileViewController.swift
+++ Instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -12,18 +12,22 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configureNavigationbar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureNavigationbar(){
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "gear"),
+            style: .done,
+            target: self,
+            action: #selector(didTapSettingButton))
     }
-    */
+    
+    @objc private func didTapSettingButton(){
+        let vc = SettingsViewController()
+        vc.title = "Settings"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
